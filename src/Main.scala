007e194142b6fcbe12ee73c05a46292bbdeaed10
Olga Borzenkova <kyssike@gmail.com>
2025-11-14 17:22:38 +0300

    better-monadic-for plugin usage example

--- src/Main.scala
+++ src/Main.scala
@@ -1,10 +1,18 @@
 package main
 
-import example.Example
 
 import java.io.IOException
-import zio.{Console, IO, ZIOAppDefault}
+import zio.{Console, IO, Task, ZIO, ZIOAppDefault}
 
 object Main extends ZIOAppDefault {
-  def run: IO[IOException, Unit] = Console.printLine(Example.text)
+
+  val example: Task[String] =
+    for {
+      (_,bar) <- get
+    } yield bar
+
+  def get: Task[(String, String)] = ZIO.attempt("foo" -> "success")
+
+  def run: IO[IOException, Unit] =
+      example.flatMap(x => Console.printLine(x)).ignore
 }

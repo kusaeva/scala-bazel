6ff525be2a91f9183059c3ff11c0c763ebc5794a
Olga Borzenkova <kyssike@gmail.com>
2026-02-05 09:57:52 +0300

    init sample project

---
+++ test/TestSpec.scala
@@ -0,0 +1,14 @@
+package com.example
+
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class TestSpec extends AnyWordSpecLike {
+
+
+  "TestSpec" should {
+
+    "run" in {
+      assert(true)
+    }
+  }
+}
